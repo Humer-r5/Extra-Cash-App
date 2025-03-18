e26aa0a4af13008780d531f4294a7c7123476efe
Your Correct GitHub Username <chaveenreddy.tx@gmail.com>
2025-03-18 19:33:47 +0530

    connections between forgot otp pages"

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screens/landing_page.dart'; // Import Splash Screen
-import 'screens/OtpVerificationScreen .dart'; // Import Splash Screen
+import 'screens/forgot-mpin.dart'; // Import Splash Screen
 
 
 void main() {
@@ -19,7 +19,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home:  OtpVerificationScreen(), // Show Splash Screen First
+      //home:  ForgotMpinScreen(), // Show Splash Screen First
+     
+      home:  SplashScreen(), // Show Splash Screen First
     );
   }
 }
