356a110b2d8847372a6039f32faf4cbe96f97d91
sandeeptechXpo <sandeepmannila.tx@gmail.com>
2025-03-25 17:09:57 +0530

    Camera Access

--- lib/screens/camera_page.dart
+++ lib/screens/camera_page.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 import 'package:camera/camera.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:gal/gal.dart';
